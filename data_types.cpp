ed3bbd64be69c143563cd2286dba4a17d45c4ec7
steimel60 <72476768+steimel60@users.noreply.github.com>
2021-05-29 18:26:23 -0400

    commit 1

---
+++ data_types.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int x = 2; //assignment operator
+  string name = "Dylan";
+  float y; //define variable type
+  y = 6.9
+  cout << name << y;
+  return 0;
+}
+
+/************************
+int 420;
+float 6.9;
+bool true, false;
+string "Hello World!"
+char 'a'
+//single line comment
+*************************/
