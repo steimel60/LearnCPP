60feb5230bb48d91f9d0d5515e5a4836a4886287
steimel60 <72476768+steimel60@users.noreply.github.com>
2021-05-30 14:37:01 -0400

    operators

---
+++ arithmetic_ops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int x = 8;
+  int y = 9;
+  int sum = x + y;
+  cout << sum;
+}
+
+
+/*******************************************
++
+-
+/
+*
+% int only
+
++=
+-=
+*=
+/=
+++
+--
+
+************************************************/
