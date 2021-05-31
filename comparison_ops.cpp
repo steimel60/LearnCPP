4ac5307a1d8f86ffbad98783d539eba34e63edb9
steimel60 <72476768+steimel60@users.noreply.github.com>
2021-05-31 16:47:22 -0400

    comparison ops

---
+++ comparison_ops.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  //bool test = 1 < 2;
+  //cout << test;
+
+  int num1, num2;
+  cout << "Type a number: ";
+  cin >> num1;
+  cout << "Enter a second number: ";
+  cin >> num2;
+  cout << "These two numbers are the same: " << (num1 == num2);
+}
+
+/*
+
+true = 1, false = 0
+
+<
+>
+<=
+>=
+!=
+==
+*/
