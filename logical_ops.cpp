475aa488ef4afc842e859da7c31fb93719d96133
steimel60 <72476768+steimel60@users.noreply.github.com>
2021-06-05 18:03:54 -0400

    logical operators

---
+++ logical_ops.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  bool x = !false; // NOT FALSE
+  bool y = true && false; //RETURNS FALSE, both must be ture to return true
+  bool z = true || false; //RETURNS TRUE IF EITHER IS TRUE
+}
+
+
+/*
+
+!
+&&
+||
+
+*/
