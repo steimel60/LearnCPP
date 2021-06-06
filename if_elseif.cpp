f194b5fec3d4f255c4d90b6cfd7f1c4f6aaf7b71
steimel60 <72476768+steimel60@users.noreply.github.com>
2021-06-06 14:09:08 -0400

    if and else if

---
+++ if_elseif.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  int x = 7;
+  if (x < 7) {
+    cout << "x is less than 7." << endl;
+    if (x < 3) {
+      cout << "x is less than 3!";
+    }
+  }
+  else if (x = 7){
+    cout << "x is equal to 7.";
+  }
+  else {
+    cout << "x is greater than 7";
+  }
+}
+
+/*
+
+*/
